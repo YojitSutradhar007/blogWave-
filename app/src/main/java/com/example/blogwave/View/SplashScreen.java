e582231ed7b7fee20232fd1dcf5046986f1834d4
SutradharYojit <jimmysuthar08@gmail.com>
2024-07-04 18:35:22 +0530

    Design login screen

---
+++ app/src/main/java/com/example/blogwave/View/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.blogwave.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.blogwave.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent Home = new Intent(SplashScreen.this, LoginScreen.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(Home);
+                finish();
+
+            }
+        }, 2500);
+    }
+}
